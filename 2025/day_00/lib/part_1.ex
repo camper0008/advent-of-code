492dff9635b3e2e1b5012c9aadc21e77fd718025
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-12-15 17:43:50 +0100

    template

---
+++ 2025/day_00/lib/part_1.ex
@@ -0,0 +1,11 @@
+defmodule Part1 do
+  defp solve(input) do
+    "part 1 remains unsolved"
+  end
+
+  def main() do
+    input = Utils.prepare_input!("input.txt")
+
+    IO.puts(solve(input))
+  end
+end
---
+++ 2025/day_00/lib/part_2.ex
@@ -0,0 +1,11 @@
+defmodule Part2 do
+  defp solve(input) do
+    "part 2 remains unsolved"
+  end
+
+  def main() do
+    input = Utils.prepare_input!("input.txt")
+
+    IO.puts(solve(input))
+  end
+end
