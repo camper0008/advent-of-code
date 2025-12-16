c16bf9502035a9e645706efeba4f6618cd00ed35
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2025-12-16 16:23:55 +0100

    day 08 part 1 possibly works, but too slow to know

--- 2025/day_08/lib/part_1.ex
+++ 2025/day_08/lib/part_1.ex
@@ -16,6 +16,10 @@ defmodule Part1 do
 
     lhs = JunctionC.connect(lhs, rhs)
 
+    if rem(depth, 10) == 0 do
+      IO.inspect(depth)
+    end
+
     if depth > 0 do
       solve([lhs, rhs] ++ without, depth - 1)
     else
@@ -23,24 +27,41 @@ defmodule Part1 do
     end
   end
 
-  defp build_circuits(x) do
-    build_circuits(x, x)
-  end
+  defp build_circuits(junction = %JunctionC{}, lookup = [%JunctionC{} | _], filter) do
+    lookup = for x <- lookup, x.junction not in filter, do: x
 
-  defp build_circuits(junction = %JunctionC{}, lookup = [%JunctionC{} | _]) do
-    theirconns = lookup |> Enum.filter(fn x -> x.junction in junction.conns end)
-    IO.inspect(theirconns)
+    theirconns =
+      lookup
+      |> Enum.filter(fn x -> x.junction in junction.conns and x.junction not in filter end)
+      |> Enum.flat_map(&build_circuits(&1, lookup, [junction.junction | filter]))
+
+    theirconns2 =
+      lookup
+      |> Enum.filter(fn x -> junction.junction in x.conns and junction.junction not in filter end)
+      |> Enum.flat_map(&build_circuits(&1, lookup, [junction.junction | filter]))
+
+    if rem(length(filter), 10) == 0 do
+      IO.inspect(length(filter))
+    end
+
+    conc = junction.conns ++ theirconns ++ theirconns2
+    [junction.junction | conc] |> Enum.uniq()
   end
 
   def main() do
-    junctions = Utils.prepare_input!("input.example.txt")
+    junctions = Utils.prepare_input!("input.txt")
 
-    junctions = solve(junctions, 10)
+    junctions =
+      solve(junctions, 1000)
 
     junctions
-    |> Enum.map(fn x -> build_circuits(x, junctions) end)
+    |> Enum.map(fn x -> build_circuits(x, junctions, []) end)
+    |> Enum.map(&Enum.sort(&1))
+    |> Enum.sort(&(length(&1) > length(&2)))
+    |> Enum.uniq()
+    |> Enum.map(&length(&1))
+    |> Enum.take(3)
+    |> Enum.reduce(&(&1 * &2))
     |> IO.inspect()
-
-    "hello"
   end
 end
